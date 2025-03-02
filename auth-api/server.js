36840c1c1a6f0be50577dd01161eba764f991cbd
Parathakorn Thanamee <6510615187@cn330>
2025-03-02 19:32:13 +0700

    remove warning

--- auth-api/server.js
+++ auth-api/server.js
@@ -13,8 +13,8 @@ const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   }).then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
